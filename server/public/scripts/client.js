670a76d143eb8542acf05a318a4380e2ae927d59
Rider <goejrider@gmail.com>
2022-10-06 12:00:06 -0500

    add function addKoala

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,31 +1,66 @@
 console.log( 'js' );
 
+let readyOrNot;
+ 
+ 
 $( document ).ready( function(){
   console.log( 'JQ' );
   // Establish Click Listeners
-  setupClickListeners()
+  // setupClickListeners()
+  $('#addButton').on('click', addKoala);
   // load existing koalas on page load
   getKoalas();
 
 }); // end doc ready
 
-function setupClickListeners() {
-  $( '#addButton' ).on( 'click', function(){
-    console.log( 'in addButton on click' );
-    // get user input and put in an object
-    // NOT WORKING YET :(
-    // using a test object
-    let koalaToSend = {
-      name: 'testName',
-      age: 'testName',
-      gender: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
-    };
-    // call saveKoala with the new obejct
-    saveKoala( koalaToSend );
-  }); 
+
+function addKoala(){
+  console.log('addingKoala');
+  if($('#readyForTransferIn').val() === 'true' ){
+    readyOrNot = true;
+  }
+  else{
+    readyOrNot = false;
+  }
+  let newKoala = {
+    name: $('#nameIn').val(),
+      age: $('#ageIn').val(),
+      gender: $('#genderIn').val(),
+      readyForTransfer: readyOrNot,
+      notes: $('#notesIn').val()
+  }
+  console.log("newKoala is", newKoala);
+  $.ajax({
+    url:'/koala',
+    method: 'POST',
+    data: newKoala
+  })
+    .then(response=>{
+      console.log('sending Koala', response);
+    })
+    .catch(err=>{
+      console.log('there was an error in addKoala', err);
+    })
 }
+
+
+// function setupClickListeners() {
+//   $( '#addButton' ).on( 'click', function(){
+//     console.log( 'in addButton on click' );
+//     // get user input and put in an object
+//     // NOT WORKING YET :(
+//     // using a test object
+//     let koalaToSend = {
+//       name: $('#nameIn').val(),
+//       age: $('#ageIn').val(),
+//       gender: $('#genderIn').val(),
+//       readyForTransfer: readyOrNot,
+//       notes: 'testName',
+//     };
+//     // call saveKoala with the new obejct
+//     saveKoala( koalaToSend );
+//   }); 
+// }
 
 function getKoalas(){
   console.log( 'in getKoalas' );
@@ -33,8 +68,8 @@ function getKoalas(){
   
 } // end getKoalas
 
-function saveKoala( newKoala ){
-  console.log( 'in saveKoala', newKoala );
-  // ajax call to server to get koalas
+// function saveKoala( newKoala ){
+//   console.log( 'in saveKoala', newKoala );
+//   // ajax call to server to get koalas
  
-}
+// }
